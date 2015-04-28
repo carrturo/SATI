89ad156c742b44b508b4afbfd50c8d210d75edc3
Carlos Banegas <cbanegas@unitec.edu>
2015-04-28 21:25:48 +0000

    Bugs Sprint I arreglados

--- app/models/clasificacion.rb
+++ app/models/clasificacion.rb
@@ -1,7 +1,7 @@
 class Clasificacion < ActiveRecord::Base
-  validates :nombre, :edadMin, :edadMax, :comentario, :presence => true
-  validates_numericality_of :edadMin, greater_than: 0
-  validates_numericality_of :edadMax, less_than_or_equal_to: 100
-  validates_numericality_of :edadMax, greater_than: :edadMin
-  validates :nombre, :uniqueness => true
+  validates :nombre, :edadMin, :edadMax, :comentario, :presence => { :message => "es un campo obligatorio." }
+  validates_numericality_of :edadMin, greater_than: 0, :message => "debe ser mayor a 0."
+  validates_numericality_of :edadMax, less_than_or_equal_to: 100, :message => "debe ser menor o igual a 100"
+  validates_numericality_of :edadMax, greater_than: :edadMin, :message => "debe ser mayor a edad mínima."
+  validates :nombre, :uniqueness => { :message => "debe ser único." }
 end
--- app/models/genero.rb
+++ app/models/genero.rb
@@ -1,4 +1,5 @@
 class Genero < ActiveRecord::Base
-  validates :nombre, :comentario, :presence => true
-  validates :nombre, :uniqueness => true
+  validates :nombre, :comentario, :presence => { :message => "es un campo obligatorio." }
+  validates :nombre, :uniqueness => { :message => "debe ser único." }
+  validates_format_of :nombre, :with =>  /\A[-a-z]+\Z/, :message => "solo acepta letras."
 end

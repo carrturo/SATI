787e8e8a9e12f4b7bb45784be40e5585c3237527
Carlos Banegas <cbanegas@unitec.edu>
2015-04-28 01:18:46 +0000

    Falta agregar color rojo a espacios, mensajes de error en español, astericos en campos obligatorios, agregar solo letras en nombre de genero

--- app/models/clasificacion.rb
+++ app/models/clasificacion.rb
@@ -3,4 +3,5 @@ class Clasificacion < ActiveRecord::Base
   validates_numericality_of :edadMin, greater_than: 0
   validates_numericality_of :edadMax, less_than_or_equal_to: 100
   validates_numericality_of :edadMax, greater_than: :edadMin
+  validates :nombre, :uniqueness => true
 end

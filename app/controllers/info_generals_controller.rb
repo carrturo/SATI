4781b350de4fe01c312b86832386cee065c9c40a
dcrux <d.crux10@gmail.com>
2015-05-24 16:28:54 +0000

    Refactorized info_generals

--- app/controllers/info_generals_controller.rb
+++ app/controllers/info_generals_controller.rb
@@ -1,40 +1,13 @@
 class InfoGeneralsController < ApplicationController
-  before_action :set_info_general, only: [:show, :edit, :update, :destroy]
-
-  # GET /info_generals
-  # GET /info_generals.json
-  def index
-    @info_generals = InfoGeneral.all
-  end
+  before_action :set_info_general, only: [:show, :edit, :update] #:destroy
 
   # GET /info_generals/1
   # GET /info_generals/1.json
   def show
   end
 
-  # GET /info_generals/new
-  def new
-    @info_general = InfoGeneral.new
-  end
-
   # GET /info_generals/1/edit
   def edit
-  end
-
-  # POST /info_generals
-  # POST /info_generals.json
-  def create
-    @info_general = InfoGeneral.new(info_general_params)
-
-    respond_to do |format|
-      if @info_general.save
-        format.html { redirect_to @info_general, notice: 'Info general was successfully created.' }
-        format.json { render :show, status: :created, location: @info_general }
-      else
-        format.html { render :new }
-        format.json { render json: @info_general.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   # PATCH/PUT /info_generals/1
@@ -48,16 +21,6 @@ class InfoGeneralsController < ApplicationController
         format.html { render :edit }
         format.json { render json: @info_general.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # DELETE /info_generals/1
-  # DELETE /info_generals/1.json
-  def destroy
-    @info_general.destroy
-    respond_to do |format|
-      format.html { redirect_to info_generals_url, notice: 'Info general was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
--- test/controllers/info_generals_controller_test.rb
+++ test/controllers/info_generals_controller_test.rb
@@ -5,25 +5,6 @@ class InfoGeneralsControllerTest < ActionController::TestCase
     @info_general = info_generals(:one)
   end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:info_generals)
-  end
-
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should create info_general" do
-    assert_difference('InfoGeneral.count') do
-      post :create, info_general: { direccion: @info_general.direccion, email: @info_general.email, fb: @info_general.fb, ig: @info_general.ig, nombre: @info_general.nombre, telefono: @info_general.telefono, tw: @info_general.tw, youtube: @info_general.youtube }
-    end
-
-    assert_redirected_to info_general_path(assigns(:info_general))
-  end
-
   test "should show info_general" do
     get :show, id: @info_general
     assert_response :success
@@ -39,11 +20,4 @@ class InfoGeneralsControllerTest < ActionController::TestCase
     assert_redirected_to info_general_path(assigns(:info_general))
   end
 
-  test "should destroy info_general" do
-    assert_difference('InfoGeneral.count', -1) do
-      delete :destroy, id: @info_general
-    end
-
-    assert_redirected_to info_generals_path
-  end
 end
